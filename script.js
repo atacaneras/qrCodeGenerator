15646cc3f74a351c3a41399e4b7729a93717eff8
Ata Can Eras <126287749+atacaneras@users.noreply.github.com>
2023-06-21 18:04:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -18,21 +18,28 @@ function generateQRCode() {
   }
   
   function downloadQRCode() {
-    var qrcodeElement = document.getElementById('qrcode');
-    var qrcodeDataURL = qrcodeElement.querySelector('img').src;
-  
-    fetch(qrcodeDataURL)
-      .then(response => response.blob())
-      .then(blob => {
-        // Create a temporary anchor element for downloading
-        var downloadLink = document.createElement('a');
-        downloadLink.href = URL.createObjectURL(blob);
-        downloadLink.download = 'qrcode.png'; // Set the file name with the .png extension
-  
-        // Append the anchor element to the document and trigger a click event
-        document.body.appendChild(downloadLink);
-        downloadLink.click();
-        document.body.removeChild(downloadLink);
-      });
-  }
-  
+  var qrcodeElement = document.getElementById('qrcode');
+  var qrcodeImage = qrcodeElement.querySelector('img');
+
+  // Create a temporary canvas element
+  var canvas = document.createElement('canvas');
+  var context = canvas.getContext('2d');
+  canvas.width = qrcodeImage.width;
+  canvas.height = qrcodeImage.height;
+
+  // Draw the QR code image on the canvas
+  context.drawImage(qrcodeImage, 0, 0);
+
+  // Convert the canvas to a data URL (PNG image)
+  var dataURL = canvas.toDataURL('image/png');
+
+  // Create a temporary anchor element for downloading
+  var downloadLink = document.createElement('a');
+  downloadLink.href = dataURL;
+  downloadLink.download = 'qrcode.png'; // Set the file name with the .png extension
+
+  // Append the anchor element to the document and trigger a click event
+  document.body.appendChild(downloadLink);
+  downloadLink.click();
+  document.body.removeChild(downloadLink);
+}
